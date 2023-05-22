1eddd92b1edf0447225463081f0cf514f9801bc6
DmcpEI <diogomcpaixao@gmail.com>
2023-05-22 13:41:49 +0200

    First Pages

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require ('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => { 
-    res.send('Hello World');
+//Acess to static files
+app.use('/public', express.static(path.join(__dirname,'public')));
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+//404 Page
+app.use((req,res) =>{
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen (PORT , () => {
