8f95e4d537a35e32afd8f5360e9a7f974b88243e
DmcpEI <diogomcpaixao@gmail.com>
2023-06-06 01:47:21 +0200

    Register e Login

--- database/users.js
+++ database/users.js
@@ -9,9 +9,8 @@ const getUserById = async (id) => {
   return await db.users.findOne({ _id: new ObjectId(id) });
 };
 
-/*
-const getUserByUsername = async (username) => {
-  return await db.users.findOne({ username: username });
+const getUserByName = async (name) => {
+  return await db.users.findOne({ name: name });
 };
 
 const createUser = async (user) => {
@@ -19,30 +18,12 @@ const createUser = async (user) => {
   return { ...user, _id: result.insertedId };
 };
 
-const updateUser = async (userId, updatedUserData) => {
-  try {
-    const result = await db.users.updateOne(
-      { _id: new ObjectId(userId) },
-      { $set: updatedUserData }
-    );
-
-    if (result.modifiedCount === 0) {
-      return null;
-    }
-
-    return { ...updatedUserData, _id: new ObjectId(userId) };
-  } catch (error) {
-    throw new Error('Failed to update user');
-  }
-};
-*/
-
 module.exports = {
   getAllUsers,
   getUserById,
-  /*
-  getUserByUsername,
+  getUserByName,
   createUser,
+  /*
   updateUser,
   */
 };
